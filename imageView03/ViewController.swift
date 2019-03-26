1d622bca29413d0fb0c5d1a1fc2ec96a746d519b
D7703_11 <d7703_11@d7703-11ui-imac.local>
2019-03-26 15:33:29 +0900

    Image-View-Circular-Play

--- imageView03/ViewController.swift
+++ imageView03/ViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    @IBOutlet weak var myLable: UILabel!
+    
+    var count = 0
+    var dir = true
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        myLable.text="0"
+        myImageView.image = UIImage(named:"frame1")
     }
 
 
+    @IBAction func changeImage(_ sender: Any) {
+        
+        if count == 5{
+            dir = false
+        }else if count == 0{
+            dir = true
+        }
+        
+        if dir == true{
+            count += 1
+        }else if dir == false{
+            count -= 1
+        }
+        
+        myImageView.image = UIImage(named: "frame\(count)")
+        myLable.text = "frame\(count).png"
+    }
 }
 
